1c890873bd4995cc23838827e5d4b66bc99bf83a
SopiaRefaldi <2110131110004@mhs.ulm.ac.id>
2024-09-02 00:06:44 +0800

    update main css dan classpage, tentangpage

--- frontend/src/pages/ClassPage.jsx
+++ frontend/src/pages/ClassPage.jsx
@@ -1,9 +1,12 @@
 import {Container, Row, Col} from "react-bootstrap"
 import {semuaKelas} from "../data/index"
 
+import FaqComponent from "../components/FaqComponent"
+
 const ClassPage = () => {
-  return  <div className="kelas min-vh-100">
-    <div className="kelas-page">
+  return  (
+<div className="kelas-page">
+  <div className="kelas min-vh-100">
     <Container>
     <Row>
       <Col>
@@ -12,32 +15,33 @@ const ClassPage = () => {
       </Col>
     </Row>
     <Row>
-                    {semuaKelas.map((kelas)=> {
-                    return (
-                    <Col key={kelas.id} className="shadow rounded">
-                    <img src={kelas.image} alt="unplash.com" className="w-100 mb-5 rounded-top" />
-                    <div className="star mb-2 px-3">
-                        <i className={kelas.star1}></i>
-                        <i className={kelas.star2}></i>
-                        <i className={kelas.star3}></i>
-                        <i className={kelas.star4}></i>
-                        <i className={kelas.star5}></i>
-                    </div>
-                    <h5 className="mb-5 px-3">{kelas.title}</h5>
-                    <div className="ket d-flex justify-content-between align-item-center px-3 pb-3">
-                        <p className="m-0 text-primary fw-bold">{kelas.price}</p>
-                        <button className="btn btn-danger rounded-2">{kelas.buy}</button>
-                    </div>
-                </Col>
+        {semuaKelas.map((kelas)=> {
+          return (
+        <Col key={kelas.id} className="shadow rounded">
+          <img src={kelas.image} alt="unplash.com" className="w-100 mb-5 rounded-top" />
+          <div className="star mb-2 px-3">
+          <i className={kelas.star1}></i>
+          <i className={kelas.star2}></i>
+          <i className={kelas.star3}></i>
+          <i className={kelas.star4}></i>
+          <i className={kelas.star5}></i>
+          </div>
+          <h5 className="mb-5 px-3">{kelas.title}</h5>
+          <div className="ket d-flex justify-content-between align-item-center px-3 pb-3">
+          <p className="m-0 text-primary fw-bold">{kelas.price}</p>
+          <button className="btn btn-danger rounded-2">{kelas.buy}</button>
+          </div>
+        </Col>
                     );
                 })}
 
-    </Row>
-  </Container>
+      </Row>
+        </Container>
 
   </div>
-    </div>
-  
+  <FaqComponent></FaqComponent>
+</div>
+  )
   }
 
 export default ClassPage
